4439c43fddd60b9a6c67795a3548c9ddb78170eb
Kashif2812 <75977863+Kashif2812@users.noreply.github.com>
2020-12-30 16:50:50 +0500

    Update Alcohol _sensor.ino

--- Alcohol _sensor/Alcohol _sensor.ino
+++ Alcohol _sensor/Alcohol _sensor.ino
@@ -12,7 +12,7 @@ void loop() {
 
 value=analogRead(A0); //reading value from arduino analog pin which is recieving value from sensor pin
 lcd.setCursor(0,0);    // setting cursor on lcd,s 0th row and 0th column
-lcd.print("value of alchohal");   // writing string on lcd
+lcd.print("value of alcohol");   // writing string on lcd
 lcd.setCursor(0,1);  // setting cursor on 0th column of 1st row 
 lcd.print(value);  //printing value on lcd
 delay(100);   //waiting  for 100 milli second 
